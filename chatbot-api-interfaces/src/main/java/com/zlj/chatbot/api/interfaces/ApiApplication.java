cfc20e391f98124cd9eb8a5ae39eaadc9b7c4744
Rivy57 <1209326449@qq.com>
2023-03-28 19:33:43 +0800

    feat:init project

---
+++ chatbot-api-interfaces/src/main/java/com/zlj/chatbot/api/interfaces/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.zlj.chatbot.api.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Rivy
+ * @description
+ * @date 2023-03-28
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/com/zlj/chatbot/api/test/ApiTest.java
@@ -0,0 +1,12 @@
+package com.zlj.chatbot.api.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Rivy
+ * @description
+ * @date 2023-03-28
+ */
+@SpringBootTest
+public class ApiTest {
+}
